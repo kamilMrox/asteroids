e87978b7b6138d125c39f87c79575a551df61ea9
kamilMrox <kamilmrox@gmail.com>
2025-11-29 03:32:16 +0100

    fixed small bug

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
     while True:
@@ -19,4 +17,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
